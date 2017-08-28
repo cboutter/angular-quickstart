1b364a6a9a71f9a171e63b09f03833989cbb9771
Christophe Boutter <boutta@gmail.com>
2017-08-28 21:06:06 +0200

    Simplify AppComponent by extracting Heroes

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,6 +10,8 @@ var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var hero_detail_component_1 = require("./hero-detail.component");
 var app_component_1 = require("./app.component");
+var heroes_component_1 = require("./heroes.component");
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -18,8 +20,9 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-        declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent],
-        bootstrap: [app_component_1.AppComponent]
+        declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent, heroes_component_1.HeroesComponent],
+        bootstrap: [app_component_1.AppComponent],
+        providers: [hero_service_1.HeroService]
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule }   from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail.component'
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component'
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: 	[ HeroService ]
+
 })
-export class AppModule { }
+export class AppModule { 
+}
